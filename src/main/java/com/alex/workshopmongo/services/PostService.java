2d4200fa2e7ff59ed8bdab667b8897c0a8bb1ade
Alexsander Kafka <alexsanderkafka2001alex@gmail.com>
2023-05-15 13:59:30 -0300

    Criado o PostService e PostRepository para obter um post por id

---
+++ src/main/java/com/alex/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.alex.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alex.workshopmongo.domain.Post;
+import com.alex.workshopmongo.repository.PostRepository;
+import com.alex.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository rep;
+
+	public Post findById(String id) {
+		Optional<Post> user = rep.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
